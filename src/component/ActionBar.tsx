bbdec70e35706312c8dd2c9fc62c6d20699201ed
ewauq <emilien.wauquier@gmail.com>
2026-04-05 20:11:47 +0200

    Remove old dark mode switch

--- src/component/ActionBar.tsx
+++ src/component/ActionBar.tsx
@@ -5,10 +5,6 @@ import './ActionBar.scss'
 const ActionBar = (): JSX.Element => {
   const { openDrawer, setOpenDrawer } = useDrawer()
 
-  const handleDarkModeClick = (): void => {
-    // todo: implement dark mode
-  }
-
   const handleDrawerMenuClick = (): void => {
     setOpenDrawer(!openDrawer)
     window.scrollTo(0, 0)
@@ -18,7 +14,6 @@ const ActionBar = (): JSX.Element => {
     <header className="action-bar">
       <button className="drawer-toggle" onClick={handleDrawerMenuClick} aria-label="Open menu" aria-expanded={openDrawer}></button>
       <span className="title">🌳 Progression Checklist ☀️</span>
-      <button className="dark-mode-toggle" onClick={handleDarkModeClick} aria-hidden="true"></button>
     </header>
   )
 }
